8131dba25547eb64e78b30a84bcc9b37f243dd86
Fope <fopeayo@hotmail.com>
2020-11-12 12:04:59 +0000

    new api changes

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,17 @@
 $( "form" ).submit(function( event ) {
   event.preventDefault();
 
-  axios.post('http://localhost:3000/quotes')
+  axios.post('http://localhost:5000/quotes')
   .then(function (response) {
     quote = response.data
     $('#text').html(quote)
   })
 
 });
+
+// fetch.get('http://localhost:3000/quotes')
+// .then(function (response) {
+//   quote = response.data
+//   $('#text').html(quote)
+// })
+// resp => resp.json()).then(console.log)
